097f52929224b5bf6caa7dcbd58400fa693fb1cc
chihiro2101 <chihiroogino2101@gmail.com>
2020-12-27 15:43:44 +0700

    up

--- Intro_DataScience/checkData.py
+++ Intro_DataScience/checkData.py
@@ -14,7 +14,8 @@ import warnings; warnings.filterwarnings(action='once')
 import random
 import math
 
-df = pd.read_csv("dfBDS.csv", dtype = {"id":"string", "month": "string"})
+# df = pd.read_csv("dfBDS.csv", dtype = {"id":"string", "month": "string"})
+df = pd.read_csv("dfBDS.csv")
 percentage_of_null = df.isnull().sum() * 100 / len(df)
 print(percentage_of_null)
 
@@ -25,9 +26,11 @@ newdf = df.drop('law_doc', axis=1)
 newdf = newdf[newdf['price'].notnull()]
 newdf = newdf.loc[(newdf['price'] != 'Thỏa thuận')]
 newdf['price'] = newdf['price'].apply(lambda x : float(x))
-newdf = newdf.loc[(newdf['price'] < 20000)]
+newdf = newdf.loc[(newdf['price'] < 10000)]
+
 newdf = newdf.loc[(newdf['square'] < 500)]
-newdf = newdf.loc[(newdf['bedrooms'] < 15)]
-newdf = newdf.loc[(newdf['bathrooms'] < 15)]
+newdf = newdf.loc[(newdf['bedrooms'] < 5)]
+newdf = newdf.loc[(newdf['bathrooms'] < 5)]
+newdf = newdf[newdf['district'].notnull()]
 newdf.to_csv (r'newdfBDS.csv', index = False, header=True)
 print("DONE")
